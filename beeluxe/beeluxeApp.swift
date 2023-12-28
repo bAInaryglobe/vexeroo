f0a087f3b0bcebe84681a299684dd8582a58a2a4
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-12-28 05:40:59 -0800

    Initial Commit

---
+++ beeluxe/beeluxeApp.swift
@@ -0,0 +1,20 @@
+//
+//  beeluxeApp.swift
+//  beeluxe
+//
+//  Created by Xcode on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct beeluxeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
