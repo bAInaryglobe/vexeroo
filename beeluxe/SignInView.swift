dbf2ce68a864cce917ff5b0c1dea25d1162512d2
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-12-31 03:02:04 -0800

    testings

--- beeluxe/SignInView.swift
+++ beeluxe/SignInView.swift
@@ -17,8 +17,8 @@ struct SignInView: View {
             HStack {
                 Image("appLogo")
                     .resizable() // Make the image resizable
-                        .aspectRatio(contentMode: .fit) // Maintain the aspect ratio and fit within the available space
-                        .frame(width: 100, height: 100) ///
+                    .aspectRatio(contentMode: .fit) // Maintain the aspect ratio and fit within the available space
+                    .frame(width: 100, height: 100) ///
                 ///logo
                 Text("beeluxe")
             }
@@ -26,13 +26,13 @@ struct SignInView: View {
             .padding()
             
             VStack {
-                Text("Sign In")
+                Text("Sign in with")
                     .font(.title)
                 
-//                TextField("Email Address", text: $email)
-//                    .padding()
-//                    .border(Color.gray, width: 0.5)
-//
+                //                TextField("Email Address", text: $email)
+                //                    .padding()
+                //                    .border(Color.gray, width: 0.5)
+                //
                 TextField("Email Address", text: $email)
                     .padding()
                     .background(Color.cyan)
@@ -44,97 +44,230 @@ struct SignInView: View {
                     .font(.title2)
                     .padding()
                 
-                Text("Sign in with")
-                    .font(.title2)
-                    .padding()
-                
-                HStack {
-                    URLImage(URL(string: "https://example.com/microsoftLogo.png")!) {
-                        // This view is displayed before download starts
-                        EmptyView()
-                    } inProgress: { _ in
-                        // Display progress
-                        ProgressView()
-                    } failure: { error, retry in
-                        // Display error and retry button
-                        VStack {
-                            Text(error.localizedDescription)
-                            Button("Retry", action: retry)
-                        }
-                    } content: { image in
-                        // Downloaded image
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }
+                //                Text("Sign in with")
+                //                    .font(.title2)
+                //                    .padding()
+//                GeometryReader { geometry in
                     
-                    // Add more buttons for other services...
+                    HStack {
+                        URLImage(URL(string: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpurepng.com%2Fpublic%2Fuploads%2Flarge%2Fpurepng.com-microsoft-logo-iconlogobrand-logoiconslogos-251519939091wmudn.png&f=1&nofb=1&ipt=b4b672318eac77d32223e20fae005eb23c3974fc5b7f74407bf55c27ee944808&ipo=images")!) {
+                            // This view is displayed before download starts
+                            EmptyView()
+                        } inProgress: { _ in
+                            // Display progress
+                            ProgressView()
+                        } failure: { error, retry in
+                            // Display error and retry button
+                            VStack {
+//                                Text(error.localizedDescription)
+                                Button("Microsoft", action: retry)
+                            }
+                        } content: { image in
+                            // Downloaded image
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30) ///
+                            
+                        }
+                        
+                        
+                        
+                        
+                        URLImage(URL(string: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.HG6XtzIxf4Nbo_vZt8T3EAHaHa%26pid%3DApi&f=1&ipt=b0ecebf62f8a5513bd98fdbb0188e018898f9ed77c8853d9a1310c27dafff9ce&ipo=images")!) {
+                            // This view is displayed before download starts
+                            EmptyView()
+                        } inProgress: { _ in
+                            // Display progress
+                            ProgressView()
+                        } failure: { error, retry in
+                            // Display error and retry button
+                            VStack {
+//                                Text(error.localizedDescription)
+                                Button("Google", action: retry)
+                            }
+                        } content: { image in
+                            // Downloaded image
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30) ///
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        URLImage(URL(string: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9g4dkKVAUyciOuDI9_vEYQHaHa%26pid%3DApi%26h%3D160&f=1&ipt=511b01aa3849848db7c1b5f386643272f0fb1ceea5d730f5f52c0eaf646b6015&ipo=images")!) {
+                            // This view is displayed before download starts
+                            EmptyView()
+                        } inProgress: { _ in
+                            // Display progress
+                            ProgressView()
+                        } failure: { error, retry in
+                            // Display error and retry button
+                            VStack {
+//                                Text(error.localizedDescription)
+                                Button("Apple", action: retry)
+                            }
+                        } content: { image in
+                            // Downloaded image
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30) ///
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        URLImage(URL(string: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.BiBzsnjVIzD2vwAqSpfq1QHaCn%26pid%3DApi&f=1&ipt=0ec8d58f8b6177fb735fede483f3851949bb0cb0b14e66049b75a2872d977f46&ipo=images")!) {
+                            // This view is displayed before download starts
+                            EmptyView()
+                        } inProgress: { _ in
+                            // Display progress
+                            ProgressView()
+                        } failure: { error, retry in
+                            // Display error and retry button
+                            VStack {
+//                                Text(error.localizedDescription)
+                                Button("Zoho", action: retry)
+                            }
+                        } content: { image in
+                            // Downloaded image
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30) ///
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        URLImage(URL(string: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.VMu8fKE6RtdnlmFWJ0cBGAHaHa%26pid%3DApi&f=1&ipt=7a3b6a1875f3f8a39e216860bc225a3503ac77a0cc4360daa69ed3fe96e95f08&ipo=images")!) {
+                            // This view is displayed before download starts
+                            EmptyView()
+                        } inProgress: { _ in
+                            // Display progress
+                            ProgressView()
+                        } failure: { error, retry in
+                            // Display error and retry button
+                            VStack {
+//                                Text(error.localizedDescription)
+                                Button("Github", action: retry)
+                            }
+                        } content: { image in
+                            // Downloaded image
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30) ///
+                            
+                        }
+                        
+                        
+                        
+                        
+                        // Add more buttons for other services...
+                    }
                 }
+                .padding()
             }
-            .padding()
         }
     }
-}
-
-
-//
-//import SwiftUI
-//
-//struct SignInView: View {
-//    @State private var email: String = ""
-//
-//    var body: some View {
-//        VStack {
-//            HStack {
-//                Image("appLogo") // Replace "appLogo" with your actual app logo
-//                Text("beeluxe")
-//            }
-//            .font(.largeTitle)
-//            .padding()
-//
-//            VStack {
-//                Text("Sign In")
-//                    .font(.title)
-//
-//                TextField("Email Address", text: $email)
-//                    .padding()
-//                    .background(Color.cyan)
-//                    .cornerRadius(10)
-//                    .shadow(radius: 10)
-//                    .padding(.bottom)
-//
-//                Text("OR")
-//                    .font(.title2)
-//                    .padding()
-//
-//                Text("Sign in with")
-//                    .font(.title2)
-//                    .padding()
-//
-//                HStack {
-//                    URLImage(URL(string: "https://example.com/microsoftLogo.png")!) {
-//                        // This view is displayed before download starts
-//                        EmptyView()
-//                    } inProgress: { _ in
-//                        // Display progress
-//                        ProgressView()
-//                    } failure: { error, retry in
-//                        // Display error and retry button
-//                        VStack {
-//                            Text(error.localizedDescription)
-//                            Button("Retry", action: retry)
-//                        }
-//                    } content: { image in
-//                        // Downloaded image
-//                        image
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                    }
-//
-//                    // Add more buttons for other services...
-//                }
-//            }
-//            .padding()
-//        }
-//    }
+    
+    
+    //
+    //import SwiftUI
+    //
+    //struct SignInView: View {
+    //    @State private var email: String = ""
+    //
+    //    var body: some View {
+    //        VStack {
+    //            HStack {
+    //                Image("appLogo") // Replace "appLogo" with your actual app logo
+    //                Text("beeluxe")
+    //            }
+    //            .font(.largeTitle)
+    //            .padding()
+    //
+    //            VStack {
+    //                Text("Sign In")
+    //                    .font(.title)
+    //
+    //                TextField("Email Address", text: $email)
+    //                    .padding()
+    //                    .background(Color.cyan)
+    //                    .cornerRadius(10)
+    //                    .shadow(radius: 10)
+    //                    .padding(.bottom)
+    //
+    //                Text("OR")
+    //                    .font(.title2)
+    //                    .padding()
+    //
+    //                Text("Sign in with")
+    //                    .font(.title2)
+    //                    .padding()
+    //
+    //                HStack {
+    //                    URLImage(URL(string: "https://example.com/microsoftLogo.png")!) {
+    //                        // This view is displayed before download starts
+    //                        EmptyView()
+    //                    } inProgress: { _ in
+    //                        // Display progress
+    //                        ProgressView()
+    //                    } failure: { error, retry in
+    //                        // Display error and retry button
+    //                        VStack {
+    //                            Text(error.localizedDescription)
+    //                            Button("Retry", action: retry)
+    //                        }
+    //                    } content: { image in
+    //                        // Downloaded image
+    //                        image
+    //                            .resizable()
+    //                            .aspectRatio(contentMode: .fit)
+    //                    }
+    //
+    //                    // Add more buttons for other services...
+    //                }
+    //            }
+    //            .padding()
+    //        }
+    //    }
+    //}
 //}
--- beeluxe/beeluxeApp.swift
+++ beeluxe/beeluxeApp.swift
@@ -5,16 +5,223 @@
 //  Created by Xcode on 12/28/23.
 //
 
+//import SwiftUI
+//
+//@main
+//struct beeluxeApp: App {
+//    let persistenceController = PersistenceController.shared
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            SignInView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//        }
+//    }
+//}
+
+
+
 import SwiftUI
 
 @main
 struct beeluxeApp: App {
     let persistenceController = PersistenceController.shared
 
+    @State private var isShowingSignInPage = false
+    @State private var opacity: Double = 0
+    @State private var rotation: Double = 0
+
+    
+//    @State private var opacity: Double = 0
+     @State private var scale: CGFloat = 0
+     @State private var swing: Bool = false
+//
+    
+    
+    
     var body: some Scene {
         WindowGroup {
-            SignInView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            
+            
+            ZStack {
+                if isShowingSignInPage {
+                    SignInView()
+                        .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                } else {
+//                    Image("welcome")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .opacity(opacity)
+//                        .frame(width: 100, height: 100)
+//                        .onAppear {
+//                            withAnimation(.easeIn(duration: 1)) {
+//                                opacity = 1
+//                            }
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                                withAnimation {
+//                                    isShowingSignInPage = true
+//                                }
+//                            }
+//                        }
+//
+//
+//
+//                    Image("welcome")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .opacity(opacity)
+//                        .frame(width: 100, height: 100)
+//                        .onAppear {
+//                            withAnimation(.easeIn(duration: 2)) {
+//                                opacity = 1
+//                            }
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                                withAnimation(.easeOut(duration: 2)) {
+//                                    opacity = 0
+//                                }
+//                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                                    isShowingSignInPage = true
+//                                }
+//                            }
+//                        }
+                    
+                    
+                    
+                    
+                    
+//                    Color.clear.overlay(
+//                        Image("welcome")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width: 100, height: 100),
+//                        alignment: .center
+//                    )
+//                    .opacity(opacity)
+//                    .onAppear {
+//                        withAnimation(.easeIn(duration: 2)) {
+//                            opacity = 1
+//                        }
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                            withAnimation(.easeOut(duration: 2)) {
+//                                opacity = 0
+//                            }
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                                isShowingSignInPage = true
+//                            }
+//                        }
+//                    }
+//
+                    
+                    
+                    
+                    
+//
+//
+                    Color.clear.overlay(
+                        Image("welcome")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .rotation3DEffect(
+                                .degrees(60),
+                                axis: (x: 1.0, y: 1.0, z: 1.0)
+                            )
+                            .shadow(color: .gray, radius: 10, x: 5, y: 5),
+                        alignment: .center
+                    )
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 4)) {
+                            opacity = 1
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                            isShowingSignInPage = true
+                        }
+                    }
+//
+//
+                    
+                    
+                    
+                    
+                    
+                    
+//                    Color.clear.overlay(
+//                        Image("welcome")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width: 300, height: 300)
+//                            .scaleEffect(scale)
+//                            .rotation3DEffect(
+//                                .degrees(swing ? 0 : 60),
+//                                axis: (x: 0.0, y: 1.0, z: 0.0)
+//                            )
+//                            .shadow(color: .gray, radius: 10, x: 5, y: 5),
+//                        alignment: .center
+//                    )
+//                    .opacity(opacity)
+//                    .onAppear {
+//                        withAnimation(.easeIn(duration: 4)) {
+//                            opacity = 1
+//                            scale = 1
+//                        }
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                            withAnimation(.easeInOut(duration: 2)) {
+//                                swing = true
+//                            }
+//                        }
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+//                            isShowingSignInPage = true
+//                        }
+//                    }
+////
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+//                    Color.clear.overlay(
+//                                Image("welcome")
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: 650, height: 650)
+//                                    .scaleEffect(scale)
+//                                    .rotationEffect(.degrees(rotation))
+//                                    .shadow(color: .gray, radius: 10, x: 5, y: 5),
+//                                alignment: .center
+//                            )
+//                            .opacity(opacity)
+//                            .onAppear {
+//                                withAnimation(.easeIn(duration: 4)) {
+//                                    opacity = 1
+//                                    scale = 1
+//                                }
+//                                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+//                                    rotation = 360
+//                                }
+//                                DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+//                                    isShowingSignInPage = true
+//                                }
+//                            }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+            }
         }
     }
 }
+
+//struct SignInView: View {
+//    var body: some View {
+//        // Your sign-in page view here
+//        Text("Sign In Page")
+//    }
+//}
